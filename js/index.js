d45290842b1f18944ea3a7b0ed6474a65a089289
Julio Bermudez <julio@pulpowms.com>
2026-05-14 12:02:19 -0400

    finish challenge

--- js/index.js
+++ js/index.js
@@ -1,2 +1,21 @@
-// Write custom JavaScript here.
-// You may ignore this file and delete if if JavaScript is not required for your challenge.
+      class EmailInput extends HTMLElement {
+        constructor() {
+          super();
+          this.attachShadow({ mode: "open" });
+        }
+        connectedCallback() {
+          this.shadowRoot.innerHTML = `
+          <style>
+            div { font-family: sans-serif; color: blue; }
+          </style>
+          <input 
+            type="email"
+            placeholder="${this.getAttribute("aria-placeholder")}"
+            aria-label="${this.getAttribute("aria-label")}"
+            hint="${this.getAttribute("hint")}"
+            />
+          `;
+        }
+      }
+
+      customElements.define("email-input", EmailInput);
